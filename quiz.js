fba0d7b87aac4f11b01d5a1edc96d291217f9262
unknown <saniaghous25>
2024-08-15 09:31:48 -0700

    first commit

---
+++ quiz.js
@@ -0,0 +1,74 @@
+alert("Certainly! Here are 10 multiple-choice questions (MCQs) about JavaScript,\nRemamber only choose a,b or c for your answer.Let's start!")
+
+var Q1 = prompt("Question #1\nWhich keyword is used to define a class in JavaScript?\na)class \nb)function  \nc) object")
+if (Q1 === "class") {
+    alert("Conratulation!\n your answer is correct")
+
+}
+else {
+    alert("wrong")
+}
+var Q2 = prompt("Question #2\nWhich of the following is a correct way to define a variable in JavaScript?\na)var variableName = value; \nb) let variableName = value; \nc) None of this")
+if (Q2 == "None of this") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q3 = prompt("Question #3\nWhat will be the output of the following code?\nconsole.log(2 + '2');\na) 4 \nb) 22 \nc) Error")
+if (Q3 == "22") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q4 = prompt("Question #4\nWhich method is used to remove whitespace from both ends of a string in JavaScript?\na) trim () \nb) strip() \nc) remove()")
+if (Q4 == "trim ()") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q5 = prompt("Question #5\nWhich of the following statements is true about JavaScript functions?\na) Functions in JavaScript are objects.\n b) Functions in JavaScript do not support default parameters.\n c) Functions in JavaScript must always return a value.")
+if (Q5 == "Functions in JavaScript are objects.") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q6 = prompt("Question #6\nWhat is the result of typeof NaN in JavaScript?\na) Number \n b) NaN\n c) Undefined")
+if (Q6 == "Number") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q7 = prompt("Question #7\nWhich of the following methods can be used to loop through the properties of an object?\na) for...in \n b) for...of\n c) While")
+if (Q7 == "for...in") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q8 = prompt("Question #8\nWhich keyword is used to create a new object in JavaScript?\na) New \n b) Creat\n c) Object")
+if (Q8 == "New") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q9 = prompt("Question #9\nWhich operator is used to compare both value and type in JavaScript?\na) == \n b) ===\n c) ==!")
+if (Q9 == "===") {
+    alert("Conratulation!\n your answer is correct")
+}
+else {
+    alert("wrong")
+}
+var Q10 = prompt("Question #10\ninside which element do we put the jasvascript?\na)<scriptting> \nb)<script> \nc<.js>")
+if (Q10 === "<script>") {
+    alert("Conratulation!\n your answer is correct")
+
+}
+else {
+    alert("wrong")
+}
